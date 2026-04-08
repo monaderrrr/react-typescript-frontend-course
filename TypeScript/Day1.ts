b5b51b0aa08746ad9ee906b4d7d744aaa041f687
Mohamed Nader <163603819+monaderrrr@users.noreply.github.com>
2026-04-08 23:12:35 +0200

    Update Day1.ts

---
+++ TypeScript/Day1.ts
@@ -0,0 +1,25 @@
+
+// TS Crash Course
+
+// LTS->Long term support
+// npm->package manger
+// yarn -> Facebook's package manager
+// عشان اعمل ال yarn علي مستوي الكمبيوتر بتاعي يعني حاجه global
+// npm i- g yarn   //-g->global
+
+// لو عاوز احمل اي باكدج من yarn
+// yarn add package-name
+//typescript ts-node بيعمل اي ده
+// 1- توفير ال compiler
+// 2-make execute to TS
+
+// package.json->hold information project
+// TypeScript ->support->JS مش العكس خد بالك
+
+//  اي الفرق بين ال dependencies و devDependencies
+// dependencies->اللي المشروع بيعتمد عليها عشان يشتغل
+// devDependencies->اللي المشروع بيعتمد عليها عشان يتطور بس مش ضروري عشان يشتغل يعني مش هيخرج ف ال production
+
+// ازاي اشغل ال Watch Mode
+// tsc اسم الملف -w
+// tsc --help او tsc -h عشان اعرف كل ال options اللي ممكن استخدمها مع ال tsc
